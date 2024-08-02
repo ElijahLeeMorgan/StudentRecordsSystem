5ced1f63e29d89437b5755f6305ed5321c5d6146
Eli <morgaeli@mail.gvsu.edu>
2024-08-02 00:05:18 -0400

    Moved delete functionaliy to a helper module.
    This codebase is getting messy, but I think I finally have the hang of Typescript + React.

--- studentrecordssystem.client/src/pages/buildings/buildings.page.tsx
+++ studentrecordssystem.client/src/pages/buildings/buildings.page.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react';
 import './buildings.scss';
 import httpModule from '../../helpers/http.module';
+import objectDeleteModule from '../../helpers/objectDelete.module';
 import { IBuilding } from '../../types/global.typing';
 import { Button, CircularProgress, ButtonGroup } from '@mui/material';
 import { Add, DeleteForever } from '@mui/icons-material';
@@ -12,30 +13,13 @@ const Buildings = () => {
     const [loading, setLoading] = useState<boolean>(false);
     const redirect = useNavigate();
 
-    //TODO Make this work on an array of IDs.
-    //TODO Move this to a helper module.
-    const deleteBuilding = (id: number) => {
-        //console.log(id);
-        httpModule
-            .delete(`/Building/Delete?id=${id}`)
-            .then(() => { //(response) =>
-                //console.log(response);
-                window.location.reload();
-            })
-            .catch((error) => {
-                alert("Error")
-                console.log(error);
-            });
-    }
-
     const handleClickDeleteButton = () => {
         const id: number = parseInt(prompt("Enter the building ID you want to delete:") || "-1");
-
         if (id <= 0) {
             alert("Please enter a valid building ID to delete");
             return;
         } else {
-            deleteBuilding(id);
+            objectDeleteModule("Building", id);
         }
     }
 
