dd8c91cd32f3abe9ff1f1d691155d48f1a3d94c5
Eli <morgaeli@mail.gvsu.edu>
2024-08-01 23:52:26 -0400

    Got the delete command to work via the frontend for Buildings.
    Currently, you can only delete one building at a time, but I'd like toadd a way to input a range of IDs and/or use MUI checkboxes.

--- studentrecordssystem.client/src/pages/buildings/buildings.page.tsx
+++ studentrecordssystem.client/src/pages/buildings/buildings.page.tsx
@@ -2,8 +2,8 @@ import {useEffect, useState} from 'react';
 import './buildings.scss';
 import httpModule from '../../helpers/http.module';
 import { IBuilding } from '../../types/global.typing';
-import { Button, CircularProgress } from '@mui/material';
-import { Add } from '@mui/icons-material';
+import { Button, CircularProgress, ButtonGroup } from '@mui/material';
+import { Add, DeleteForever } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import { BuildingsGrid } from '../../components/Buildings/buildingsGrid.component';
 
@@ -11,6 +11,33 @@ const Buildings = () => {
     const [buildings, setBuildings] = useState<IBuilding[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const redirect = useNavigate();
+
+    //TODO Make this work on an array of IDs.
+    //TODO Move this to a helper module.
+    const deleteBuilding = (id: number) => {
+        //console.log(id);
+        httpModule
+            .delete(`/Building/Delete?id=${id}`)
+            .then(() => { //(response) =>
+                //console.log(response);
+                window.location.reload();
+            })
+            .catch((error) => {
+                alert("Error")
+                console.log(error);
+            });
+    }
+
+    const handleClickDeleteButton = () => {
+        const id: number = parseInt(prompt("Enter the building ID you want to delete:") || "-1");
+
+        if (id <= 0) {
+            alert("Please enter a valid building ID to delete");
+            return;
+        } else {
+            deleteBuilding(id);
+        }
+    }
 
     useEffect(() => {
         setLoading(true);
@@ -29,13 +56,18 @@ const Buildings = () => {
 
     //console.log(buildings);
 
+    // I would make these MUI IconButtons but, it wouldn't fit the theme of the app. Plus, I'd have to play around with the darkmode styles.
     return (<div className="content buildings">
         <div className="heading">
             <h2>Buildings</h2>
-
-            <Button variant="outlined" onClick={() => redirect("/buildings/add")}>
-                <Add />
-            </Button>
+            <ButtonGroup variant="contained"> 
+                <Button onClick={() => redirect("/buildings/add")}>
+                    <Add />
+                </Button>
+                <Button color="error" onClick={() => handleClickDeleteButton()}>
+                    <DeleteForever />
+                </Button>
+            </ButtonGroup>
         </div>
         {loading ?
               <CircularProgress size={100}></CircularProgress>
